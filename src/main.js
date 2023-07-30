b6bb8132dc945bf1bc75b721aec57ce4ead73809
Rafaelsilva <99143713+Rafazg@users.noreply.github.com>
2023-07-30 15:06:31 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,143 +15,7 @@ canvas.height = window.innerHeight;
 
 const gravity = 0.5;
 
-/* class Player {
-  constructor() {
-    this.position = {
-      x: 200,
-      y: 200,
-    };
-    this.velocity = {
-      x: 0,
-      y: 1,
-    };
-    this.width = 150;
-    this.height = 150;
 
-    this.sprites = {
-      stand: {
-        right: new Image(),
-        left: new Image(),
-      },
-      run: {
-        right: new Image(),
-        left: new Image(),
-      },
-      isJump: {
-        jump: new Image(),
-      },
-      atack:{
-        right: new Image(),
-      }
-    };
-
-    // Carregue as imagens
-    
-    this.sprites.stand.right.src = "/assets/images/player/2 Punk/Punk_idle.png";
-    this.sprites.stand.left.src = "/assets/images/player/2 Punk/Punk_idleLeft.png"
-    this.sprites.run.right.src = "/assets/images/player/2 Punk/Punk_run.png";
-    this.sprites.run.left.src = "/assets/images/player/2 Punk/Punk_runLeft.png";
-    this.sprites.isJump.jump.src = "/assets/images/player/2 Punk/Punk_doublejump.png";
-
-    this.sprites.atack.right.src = "assets/images/player/2 Punk/Punk_attack3.png"
-
-
-
-    this.currentSprite = this.sprites.stand.right;
-    this.frames = 0;
-    this.currentState = "stand"; // Estado inicial do jogador é "parado"
-    this.isJumping = false; // Indica se o jogador está pulando ou não
-  }
-
-  draw() {
-    c.drawImage(
-      this.currentSprite,
-      48 * this.frames,
-      0,
-      48,
-      48,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
-  }
-
-  update() {
-    this.frames++;
-    if (this.frames > 5) this.frames = 0;
-
-    // Lógica para atualizar o estado do jogador
-    
-
-    this.draw();
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
-
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
-    }
-  }
-} */
-
-
-
-
-
-
-
-
-
-
-/* class Plataform {
-  constructor({ x, y }) {
-    this.position = {
-      x,
-      y,
-    };
-    this.width = 500;
-    this.height = 200;
-  }
-
-  draw() {
-    c.drawImage(
-      plataformImage,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
-  }
-} */
-
-/* class Bullet {
-  constructor(x, y) {
-    this.position = {
-      x,
-      y,
-    };
-    this.velocity = {
-      x: 10, // Defina a velocidade horizontal da bala
-      y: 0, // Defina a velocidade vertical da bala (se necessário)
-    };
-    this.width = 10; // Defina a largura da bala
-    this.height = 5; // Defina a altura da bala
-  }
-
-  draw() {
-    c.fillStyle = "red"; // Cor da bala
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x; // Atualiza a posição horizontal da bala
-    // Atualize a posição vertical da bala (se necessário)
-  }
-
-} */
 
 const enemyPositions = [
   { x: 500, y: 300 },
@@ -319,7 +183,7 @@ function animate(currentTime) {
         }
       }
 
-      // Remova as balas que saem da tela (caso necessário)
+     
       if (bullet.position.x > canvas.width) {
         const index = bullets.indexOf(bullet);
         if (index > -1) {
